19738f226fbf285e731d0920432ee5137852b32e
borodyanskayake <borodyanskaya.ke@gmail.com>
2025-11-30 18:12:48 +0300

    Update task02.jl

--- src/task02.jl
+++ src/task02.jl
@@ -1,10 +1,20 @@
 function arraysum(ar::AbstractVector{<:Number})::Float64
-    sum = 0;
+    sum = 0
+    for i in ar
+        sum += i
+    end
     return sum;
 end
 
 
 function mul35sum(N::Integer)::Int64
-    sum = Int64(0);
+    sum = 0
+    i = 1
+    while i < N
+        if (i % 3 == 0) || (i % 5 == 0)
+            sum += i
+        end
+        i += 1
+    end
     return sum;
 end
